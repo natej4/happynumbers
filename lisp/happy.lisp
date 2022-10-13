dd08a2664146a421907e9d6c5d1ec738edcc1fca
Nate Jackson <11004247@live.mercer.edu>
2022-10-12 21:23:33 -0400

    lisp done

--- lisp/happy.lisp
+++ lisp/happy.lisp
@@ -1,27 +1,41 @@
 #!/usr/bin/sbcl --script
+;;;;Asks the user for two positive integers
+;;;;and returns the 10 happy numbers between them
+;;;;with the greatest norms, sorted descending by the norms
+
+;;;;by Nate Jackson for CSC330
 
 (progn
+    ;;globals
     (defvar x)
     (defvar y)
     (defvar nums (make-array 1000))
     (defvar sum)
     (defvar happies (make-array 1000))
     (let ((c 0))
-
+    ;;input
     (princ "Enter first argument: ")
     (terpri)
     (setq x (read))
     (princ "Enter second argument: ")
     (terpri)
     (setq y (read))
-
+    ;; exit case when numbers are the same
+    (when (= x y)
+        (princ "NOBODYS HAPPY")
+        (terpri)
+        (EXIT))
+    (when (> x y)
+        (rotatef x y)
+        )
+;;; filling array with nums between entered values
     (loop for a from x to y do(
         progn 
         (setf (aref nums c) a)
         (setq c(+ c 1))))
-    ;; (dotimes (i c)
-    ;;     (print (aref nums i)))
-    
+   
+;;; function to determine if a number is happy
+;;; (hand written btw, not easy)
     (defun isHappy(number)
         (let ((cache (make-array 1000))(digit)(count 0)(sum))
             (setq sum 0)
@@ -29,13 +43,10 @@
             (loop
                 (unless (> number 1)
                     (return-from isHappy T))
-                ;; (print (aref cache count))
                 (dotimes (i (1- count))
                     (when (= (aref cache i) number)
                         (return-from isHappy nil)))
 
-                ;; (when (= (aref cache count) 0)
-                ;;     (return-from isHappy nil))
                 (setf (aref cache count) number)
                 (setq count (1+ count))
                 (loop 
@@ -48,17 +59,19 @@
                 
                 (setq number sum)
                 (setq sum 0))))
+
 ;;; adapted from https://literateprograms.org/bubble_sort__lisp_.html 
-    (defun bubble-sort (happyarray normarray cmp-fun) 
-  ;;"Bubble sort implementation in common lisp. Using the extended loop facility."
-    (let ((result (copy-seq happyarray))(norms (copy-seq normarray)))
+    (defun bubble-sort (array array2 cmp-fun) 
+  "Bubble sort implementation in common lisp. Using the extended loop facility."
+  (let ((result (copy-seq array)) (output (copy-seq array2)))
     (loop for i from (1- (length result)) downto 0 do
         (loop for j from 0 to i
-            when (funcall cmp-fun (aref norms i) (aref norms j))
-               do (rotatef (aref result i) (aref result j)) ))
-    result))
+            when (funcall cmp-fun (aref result i) (aref result j))
+               do (rotatef (aref result i) (aref result j)) 
+               (rotatef (aref output i) (aref output j))))
+    output))
 
-
+    ;; finding happies within nums, putting into happies array
     (let ((numHappies 0))
     (dotimes (i c)
         (when (isHappy (aref nums i))
@@ -66,7 +79,12 @@
             (setf (aref happies numHappies) n))
             (setq numHappies (1+ numHappies)))
             )
-
+    ;; exit case when no happies exist
+    (when (= numHappies 0)
+        (princ "NOBODYS HAPPY")
+        (terpri)
+        (EXIT))
+;;; function to find the norm of a happy number
     (defun norm (number)
     (let ((cache (make-array 1000))(digit)(count 0)(sum))
             (setq sum 0)
@@ -74,13 +92,10 @@
             (loop
                 (unless (> number 1)
                     (return 0))
-                ;; (print (aref cache count))
                 (dotimes (i (1- count))
                     (when (= (aref cache i) number)
                         (return 0)))
 
-                ;; (when (= (aref cache count) 0)
-                ;;     (return-from isHappy nil))
                 (setf (aref cache count) number)
                 (setq count (1+ count))
                 (loop 
@@ -94,22 +109,32 @@
                 (setq number sum)
                 (setq sum 0))
         (let ((x)(result))
+        (setq result 0)
+        (setq x 0)
         (dotimes (j count)
             (setq x (* (aref cache j) (aref cache j)))
             (setq result (+ result x)))
+
         (setq result (sqrt result))
         result)))
 
+    ;; filling a parallel array with the norm of each happy        
     (let ((normarray (make-array 1000)))
+    
     (dotimes (k numHappies)
         (let ((normNum))
             (setq normNum (norm (aref happies k)))
             (setf (aref normarray k) normNum)))
-        
-    (setf happies (bubble-sort happies normarray '>)))
-    (dotimes (j numHappies)
-            (print (aref happies j)))
-        
-    ))
     
+    ;; sorting happies array by normarray
+    (setf happies (bubble-sort normarray happies '>)))
+
+    ;; only allowing top 10 to be printed
+    (when (> numHappies 10)
+        (setq numHappies 10))
+    
+    ;;output
+    (dotimes (j numHappies)
+            (print (aref happies j)))))
+
     (write-line ""))
--- lisp/test.lisp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sbcl --script
-
-(defvar x 10)
-(when (> x 11)
-    (print "greater than"))
-
-(terpri ) 
